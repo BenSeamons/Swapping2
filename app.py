56a0b71782c8dbdca755eee3fe2b7145c7baa9fe
BenSeamons <112534432+BenSeamons@users.noreply.github.com>
2025-06-05 15:55:25 -0400

    Fix Google credentials loading

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from googleapiclient.errors import HttpError
 import pandas as pd
 import traceback
+import json
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-SERVICE_ACCOUNT_FILE = json.loads(os.environ.get('GOOGLE_CREDENTIALS'))  # path to your JSON key
 
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+
+GOOGLE_CREDENTIALS = os.environ.get("GOOGLE_CREDENTIALS")
 
 
 def get_google_sheet(sheet_id, range_name):
     try:
-        creds = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        if not GOOGLE_CREDENTIALS:
+            raise RuntimeError("GOOGLE_CREDENTIALS environment variable not set")
+        creds_info = json.loads(GOOGLE_CREDENTIALS)
+        creds = service_account.Credentials.from_service_account_info(
+            creds_info, scopes=SCOPES)
         service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
         result = sheet.values().get(spreadsheetId=sheet_id, range=range_name).execute()
